df11e711393b9b03f0d6f99468bf520e7710ab6b
Ruslan Seferov <91841957+ruslansfrov236@users.noreply.github.com>
2023-11-16 09:43:03 +0400

    Add files via upload

---
+++ Task-9/Task-9/Program.cs
@@ -0,0 +1,16 @@
+﻿using Task_9.customer;
+
+Student tural = new Student();
+Console.WriteLine(tural.Age); 
+
+Student suleyman = new Student();
+Console.WriteLine(suleyman.Age);
+
+Student gafar = new Student();
+Console.WriteLine(gafar.Age);
+
+Student semed = new Student();
+Console.WriteLine(semed.Age); 
+
+
+
---
+++ Task-9/Task-9/customer/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9.customer
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        private static byte ageCounter = 0;
+        private byte age;
+
+        public byte Age
+        {
+            get { return age; }
+        }
+
+        public Student()
+        {
+            ageCounter++;
+            age = ageCounter;
+        }
+    }
+}
